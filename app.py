9e702edbb39f18a00bcbd895ee74efd141c7c055
Sabik-adri <sabikhasan33@gmail.com>
2023-06-05 01:38:55 +0600

    updated

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def video_feed():
     return Response(detect_faces(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
